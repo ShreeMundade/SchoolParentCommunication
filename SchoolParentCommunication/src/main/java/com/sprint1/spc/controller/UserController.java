37f0d003793e71b154e750597a5829a8af92d918
Yash P <yashpatekar18@gmail.com>
2021-06-14 10:44:04 +0530

    UserController changes.

--- SchoolParentCommunication/src/main/java/com/sprint1/spc/controller/UserController.java
+++ SchoolParentCommunication/src/main/java/com/sprint1/spc/controller/UserController.java
@@ -1,19 +1,27 @@
 package com.sprint1.spc.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sprint1.spc.entities.Parent;
 import com.sprint1.spc.entities.User;
 import com.sprint1.spc.exception.EmailAlreadyExistsException;
+import com.sprint1.spc.exception.FieldErrorMessage;
 import com.sprint1.spc.exception.UserNotFoundException;
 import com.sprint1.spc.services.IParentService;
 import com.sprint1.spc.services.IUserService;
@@ -30,10 +38,18 @@ public class UserController {
 	@Autowired
 	private IParentService parentService;
 	
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    List<FieldErrorMessage> exceptionHandler(MethodArgumentNotValidException e) {
+      
+        List<FieldError> fieldErrors = e.getBindingResult().getFieldErrors();
+        List<FieldErrorMessage> fieldErrorMessages = fieldErrors.stream().map(fieldError -> new FieldErrorMessage(fieldError.getField(),fieldError.getDefaultMessage())).collect(Collectors.toList());
+        return fieldErrorMessages;
+    }
 	
 	@PostMapping("/register/parent")
 	@ApiOperation(value = "Parent Registration", notes = "Parents can register with us from here.")
-	public ResponseEntity<User> registerUser(@RequestBody Parent newUser) throws EmailAlreadyExistsException{
+	public ResponseEntity<User> registerUser(@Valid @RequestBody Parent newUser) throws EmailAlreadyExistsException{
 
 		User user = userService.getUserEmail(newUser.getEmailId());
 
@@ -51,7 +67,7 @@ public class UserController {
 
 	@PostMapping("/login")
 	@ApiOperation(value = "User Login", notes = "Users can sign in from here.")
-	public ResponseEntity<User> loginUser(@RequestBody User user) throws UserNotFoundException{
+	public ResponseEntity<User> loginUser(@Valid @RequestBody User user) throws UserNotFoundException{
 		
 		User userExists = userService.signIn(user);
 		
@@ -67,7 +83,7 @@ public class UserController {
 
 	@PostMapping("/logout")
 	@ApiOperation(value = "User Logout", notes = "Users can sign out from here.")
-	public ResponseEntity<User> logoutUser(@RequestBody User user){
+	public ResponseEntity<User> logoutUser(@Valid @RequestBody User user){
 		
 		User userInSystem = userService.signOut(user);
 		
